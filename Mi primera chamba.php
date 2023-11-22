e248af1bab40bc8686530a10980647f498653280
Santiago Tomás Leonardi <47321301+st3101@users.noreply.github.com>
2023-11-22 10:33:07 -0300

    Add files via upload

---
+++ Mi primera chamba.php
@@ -0,0 +1,55 @@
+<?php
+
+//require __DIR__ . '/../src/app/app.php';
+//require __DIR__ . '/../src/app/appLogin.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mi Sitio Web Personal</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .container {
+            width: 80%;
+            margin: auto;
+        }
+        header {
+            background: #50a3a2;
+            color: white;
+            padding: 10px;
+        }
+        main {
+            margin: 15px 0;
+        }
+        footer {
+            background: #50a3a2;
+            color: white;
+            text-align: center;
+            padding: 10px;
+            position: center;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Mi Biografía</h1>
+        </header>
+        <main>
+            <h2>Sobre mí</h2>
+            <p>Soy buena mula trabajo para el auto y la compu</p>
+            <h2>Mi currículum</h2>
+            <p>https://www.youtube.com/watch?v=e9t61Swzyfo</p>
+            <h2>Contacto</h2>
+            <p>Me pueden encontrar en discord 24/7</p>
+        </main>
+        <footer>
+            <p>© 2023 Mi Sitio Web Personal</p>
+        </footer>
+    </div>
+</body>
+</html>
