7c06909eb5f8c5c6e6e608d24aa4dac5a30de3a0
Santiago Tomás Leonardi <47321301+st3101@users.noreply.github.com>
2024-02-19 13:07:48 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mi Sitio Web Personal</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .container {
+            width: 80%;
+            margin: auto;
+        }
+        header {
+            background: #50a3a2;
+            color: white;
+            padding: 10px;
+        }
+        main {
+            margin: 15px 0;
+        }
+        footer {
+            background: #50a3a2;
+            color: white;
+            text-align: center;
+            padding: 10px;
+            position: center;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Mi Biografía</h1>
+        </header>
+        <main>
+            <h2>Sobre mí</h2>
+            <p>Soy buena mula trabajo para el auto y la compu</p>
+            <h2>Mi currículum</h2>
+            <p><a href="https://www.youtube.com/watch?v=e9t61Swzyfo"target="_blank">Ir a mi primera chamba</a></p>
+            <h2>Contacto</h2>
+            <p>Me pueden encontrar en discord 24/7</p>
+        </main>
+        <footer>
+            <p>© 2023 Mi Sitio Web Personal</p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ index3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Mi Currículum</title>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>Mi Biografía</h1>
+        </header>
+        <main>
+            <h2>Sobre mí</h2>
+            <p>Soy buena mula trabajo para el auto y la compu</p>
+            <h2>Mi currículum</h2>
+            <p><a href="https://www.youtube.com/watch?v=--1hVeRe6do"target="_blank">Ir a mi primera chamba</a></p>
+            <h2>Contacto</h2>
+            <p>Me pueden encontrar en discord 24/7</p>
+        </main>
+        <footer>
+            <p>© 2023 Mi Sitio Web Personal</p>
+        </footer>
+    </div>
+</body>
+</html>
+
+
